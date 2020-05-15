0cee8ad9b3dd181817dcfe3fafb0dd0add552fb9
wida <wida@foxmail.com>
2020-05-15 22:32:09 +0800

    更新测试程序

--- example/main.go
+++ example/main.go
@@ -45,7 +45,6 @@ func appendResp(b []byte, status, head, body string) []byte {
 	b = append(b, ' ')
 	b = append(b, status...)
 	b = append(b, '\r', '\n')
-	b = append(b, "Server: gnet\r\n"...)
 	b = append(b, "Date: "...)
 	b = time.Now().AppendFormat(b, "Mon, 02 Jan 2006 15:04:05 GMT")
 	b = append(b, '\r', '\n')
@@ -64,7 +63,7 @@ func appendResp(b []byte, status, head, body string) []byte {
 
 func main() {
 
-	l, err := net.Listen("tcp", ":9999")
+	l, err := net.Listen("tcp", "127.0.0.1:9999")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -72,53 +71,40 @@ func main() {
 	for {
 		conn, _ := l.Accept()
 		headle := func(conn net.Conn) {
-			fmt.Printf("remote addr %s", conn.RemoteAddr())
+			fmt.Printf("remote addr %s \n", conn.RemoteAddr())
 			buf := bufio.NewReader(conn)
-
 			for {
 				b, err := buf.Peek(4)
 				if len(b) == 0 {
-					log.Printf("%s", err)
+					log.Printf("%s \n", err)
 					if err == io.EOF {
-
+						conn.Close()
 						return
 					}
 
 					if err == nil {
 						panic("bufio.Reader.Peek() returned nil, nil")
 					}
-
-					// n == 1 on the first read for the request.
-					/* if n == 1 {
-						// We didn't read a single byte.
-						return errNothingRead{err}
-					} */
-
 					return
 				}
 				rq := RequestHeader{
 					Request: &httparse.Request{},
 				}
 
-				fmt.Println("---", buf.Buffered())
 				b, err = buf.Peek(buf.Buffered())
 				if err != nil {
 					log.Fatal(err)
 				}
-				//	fmt.Printf("%s\n", b)
 
 				n, err := rq.Parse(b)
 				if err != nil {
 					log.Printf("%#v \n", err)
 					return
 				}
-				fmt.Println(n)
-				fmt.Printf("%s", rq)
-
 				buf.Discard(n)
-
-				bff := make([]byte, 1024)
-				conn.Write(appendHandle(bff, "ddd"))
+				var bff []byte
+				ret := appendHandle(bff, "ddd")
+				conn.Write(ret)
 			}
 
 			//conn.Close()
