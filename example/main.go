f6b5e6cfb095f276e731c1862821ee261ea2d59f
wida <wida@foxmail.com>
2020-05-15 21:30:52 +0800

    测试

--- example/main.go
+++ example/main.go
@@ -1,12 +1,66 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"strconv"
+	"time"
 
+	"github.com/pkg/errors"
 	"github.com/widaT/httparse"
 )
+
+type RequestHeader struct {
+	*httparse.Request
+	noHTTP11        bool
+	connectionClose bool
+	contentLength   int
+	host            []byte
+	contentType     []byte
+	userAgent       []byte
+}
+
+func (r *RequestHeader) Read(buf *bufio.Reader) error {
+	b, err := buf.Peek(buf.Buffered())
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	n, err := r.Parse(b)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	buf.Discard(n)
+	return nil
+}
+func appendHandle(b []byte, res string) []byte {
+	return appendResp(b, "200 OK", "", res)
+}
+
+func appendResp(b []byte, status, head, body string) []byte {
+	b = append(b, "HTTP/1.1"...)
+	b = append(b, ' ')
+	b = append(b, status...)
+	b = append(b, '\r', '\n')
+	b = append(b, "Server: gnet\r\n"...)
+	b = append(b, "Date: "...)
+	b = time.Now().AppendFormat(b, "Mon, 02 Jan 2006 15:04:05 GMT")
+	b = append(b, '\r', '\n')
+	if len(body) > 0 {
+		b = append(b, "Content-Length: "...)
+		b = strconv.AppendInt(b, int64(len(body)), 10)
+		b = append(b, '\r', '\n')
+	}
+	b = append(b, head...)
+	b = append(b, '\r', '\n')
+	if len(body) > 0 {
+		b = append(b, body...)
+	}
+	return b
+}
 
 func main() {
 
@@ -18,19 +72,56 @@ func main() {
 	for {
 		conn, _ := l.Accept()
 		headle := func(conn net.Conn) {
-			buf := make([]byte, 30*1024)
-			n, _ := conn.Read(buf)
-			r := httparse.Request{}
-			num, err := r.Parse(buf[:n])
-			if err != nil {
-				log.Fatalf("%#v \n", err)
-				return
-			}
-			fmt.Println(n, num)
-			fmt.Printf("%s", r)
+			fmt.Printf("remote addr %s", conn.RemoteAddr())
+			buf := bufio.NewReader(conn)
 
-			conn.Write([]byte("HTTP/1.1 \r\n 200 Ok\r\n\r\n"))
-			conn.Close()
+			for {
+				b, err := buf.Peek(4)
+				if len(b) == 0 {
+					log.Printf("%s", err)
+					if err == io.EOF {
+
+						return
+					}
+
+					if err == nil {
+						panic("bufio.Reader.Peek() returned nil, nil")
+					}
+
+					// n == 1 on the first read for the request.
+					/* if n == 1 {
+						// We didn't read a single byte.
+						return errNothingRead{err}
+					} */
+
+					return
+				}
+				rq := RequestHeader{
+					Request: &httparse.Request{},
+				}
+
+				fmt.Println("---", buf.Buffered())
+				b, err = buf.Peek(buf.Buffered())
+				if err != nil {
+					log.Fatal(err)
+				}
+				//	fmt.Printf("%s\n", b)
+
+				n, err := rq.Parse(b)
+				if err != nil {
+					log.Printf("%#v \n", err)
+					return
+				}
+				fmt.Println(n)
+				fmt.Printf("%s", rq)
+
+				buf.Discard(n)
+
+				bff := make([]byte, 1024)
+				conn.Write(appendHandle(bff, "ddd"))
+			}
+
+			//conn.Close()
 		}
 
 		go headle(conn)
